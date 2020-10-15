5544084de91f420bce8d8addf6c6d6692823ea36
JiajunLi229 <https://github.com/JiajunLi229>
2020-10-16 01:22:18 +0800

    finished all practices

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,6 @@
+import { ADDRESS, PORT, PATH } from "./constant";
+import { getURL } from "./utils";
 
+const url = getURL(ADDRESS, PORT, PATH);
+export { REGION as region } from "./constant";
+export { url };
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,18 @@
-export const getPoetry
+export const getPoetry = () => {
+  const url = "https://v1.jinrishici.com/all.json";
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      accept: "text/html",
+    },
+    credentials: "same-origin",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const result = [];
+      result.push(data.origin);
+      result.push(data.author);
+      result.push(data.content);
+      return result;
+    });
+};
